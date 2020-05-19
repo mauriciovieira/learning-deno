1e541f54d041ed4c8035798998c1f4313f2be91b
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-05-19 07:25:33 -0300

    Add verbose_cat example

---
+++ mauriciovieira.net/basic/verbose_cat.ts
@@ -0,0 +1,8 @@
+// Copyright 2020 Mauricio Vieira. All rights reserved. MIT license.
+const filenames = Deno.args;
+for (const filename of filenames) {
+  const file = await Deno.open(filename);
+  console.log(`Printing ${filename}`)
+  await Deno.copy(file, Deno.stdout);
+  file.close();
+}
